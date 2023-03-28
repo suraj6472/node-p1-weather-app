1dcee1bf33fc5c7cf28289138572f26d7b179383
Suraj Kumar <suraj6472@gmail.com>
2023-03-28 15:48:54 +0530

    Section 8 done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
@@ -40,10 +43,32 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
-  res.send({
-    forecast: "It is snowing",
-    location: "Philadelphia",
-  });
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
 });
 
 app.get("/help/*", (req, res) => {
